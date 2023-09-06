5c05294c6b1e36977fdb8a03886b1f31139c349b
Owain Beynon <54601295+owainbeynon@users.noreply.github.com>
2023-09-06 17:05:11 +0100

    adding ase wrappers and examples

---
+++ package_name/ase_wrapper.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+from ase.visualize.plot import plot_atoms
+
+# TODO: - Add function to retrieve atom properties from UUID
+#       - Visualize UUID with native ASE GUI
+#       - Integrate data properties with CUDS
+#       - For adding a UUID to an atom objet, a way to add UUID to the whole atom (group) as well as individual atoms in molecule is needed
+
+
+class ASEWrapper:
+
+    def __init__(self):
+        self._mapper = {}
+        self._mapped_atoms = 0
+
+    def add_uuid_to_atoms(self, atoms, uuids):
+
+        """Add the predefined UUIDs as properties to the atoms object's info dictionary"""
+        atoms.info['uuids'] = uuids
+
+    def print_uuid(self, atoms):
+
+        """Print UUID as well as corresponding symbol and atom index of Atoms object """
+
+        molecule_symbol = atoms.get_chemical_formula()
+        print(f"Group: Symbol = {molecule_symbol}")
+        for i, atom in enumerate(atoms):
+            atom_symbol = atom.symbol
+            atom_uuid = atoms.info['uuids'][i]
+            print(f"Atom {i}: Symbol = {atom_symbol}, UUID = {atom_uuid}")
+
+    def view_uuids(self, atoms):
+
+        """View Atoms object and corresponding UUID with ASE plot_atoms"""
+
+        # Create a Matplotlib figure and axis
+        fig, ax = plt.subplots()
+
+        # Plot the atoms
+        plot_atoms(atoms, ax, radii=0.3)
+
+        # Extract UUIDs from the info dictionary and add them as annotations
+        uuids = atoms.info.get('uuids', [])
+        for i, atom in enumerate(atoms):
+            atom_position = atom.position
+            atom_uuid = uuids[i] if i < len(uuids) else ''
+            annotation_text = f"UUID: {atom_uuid}"  # Customize the annotation as needed
+            ax.text(atom_position[0], atom_position[1], annotation_text, fontsize=8, ha='center', va='center')
+
+        # Show the plot
+        plt.show()
---
+++ test_ase_wrapper.py
@@ -0,0 +1,21 @@
+from package_name.ase_wrapper import ASEWrapper
+from ase import Atoms
+
+'''Simple example of working with UIDs and ASE, where you can assign a UUID to 
+an Atom object, print UUID and then visualize it'''
+
+
+
+atoms = Atoms('H2', positions=[(0, 0, 0), (1, 1, 1)])
+uuids = ['290ad2a7-2fcb-4da3-8669-a43493b3f915', '290ad2a7-2fcb-4da3-8669-a43493b3f916']
+
+# OB 06/09/23: This all has the wrong naming, but I'm committing what I've done today
+
+ase_wrapper = ASEWrapper()
+
+ase_wrapper.add_uuid_to_atoms(atoms, uuids)
+
+ase_wrapper.print_uuid(atoms)
+
+ase_wrapper.view_uuids(atoms)
+
